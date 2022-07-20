9aab61841e4e577d11ac77452b546eab01b79ca4
CloudyyUw <cloudyy.uw@tuta.io>
2022-07-20 19:05:10 -0300

    moving API to backend/

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,20 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Manager, Menu, Submenu};
+
 fn main() {
+  let server_menu = Submenu::new(
+    "Server",
+    Menu::with_items([CustomMenuItem::new("Start Server", "Start Server").into()]),
+  );
+
   tauri::Builder::default()
+    .menu(Menu::new().add_submenu(server_menu))
+    .on_menu_event(|event| match event.menu_item_id() {
+      "Start Server" => {}
+      _ => {}
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
